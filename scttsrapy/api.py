38755c1746638807fec9381452eb163d4bd5a29c
Keiron O'Shea <keiron.oshea@gmail.com>
2022-01-28 12:47:48 +0000

    Added branch functionality.

--- scttsrapy/api.py
+++ scttsrapy/api.py
@@ -1,9 +1,53 @@
-import requests
+class EndpointBuilder():
+    _branch = "MAIN"
+    _api_endpoint = "https://snowstorm.ihtsdotools.org/snowstorm/snomed-ct/"
+    _headers = {"Accept": "application/json"}
 
-from . import *
+    def __init__(self):
+        pass
 
-def get_branches():
-    response = requests.get(_API_ENDPOINT + "branches/")
+    @property
+    def api_endpoint(self) -> str:
+        return self._api_endpoint
     
-    if response.status_code == 200:
-        print(response.json())
+    @property
+    def branch(self) -> str:
+        return self._branch
+
+    @property
+    def headers(self) -> dict:
+        return self._headers
+
+    def with_parameters(self, url: str, parameters: dict = {}):
+        if len(parameters.keys()) >= 1:
+            url += "?"
+
+        for parameter, value in parameters.items():
+            if type(value) == str:
+                parameter_str = "%s=%s&" % (parameter, value)
+            elif type(value) == bool:
+                parameter_str = "%s=%s&" % (parameter, str(value).lower())
+            elif type(value) == int:
+                parameter_str = "%s=%s&" % (parameter, str(value))
+
+            url += parameter_str
+        return url
+
+    def set_api_endpoint(self, api_url: str,) -> None:
+        if not api_url.endswith("/"):
+            api_url += "/"
+        self._api_endpoint = api_url
+
+    def set_branch(self, branch: str) -> None:
+        self._branch = branch
+
+    def set_headers(self, headers: dict) -> None:
+        self._headers.update(headers)
+
+    @property
+    def base_branch_url(self) -> str:
+        return self.api_endpoint + "branches/"
+
+    @property
+    def branch_url(self) -> str:
+        return self.base_branch_url + "%s" % (str(self.branch))
---
+++ scttsrapy/branching.py
@@ -0,0 +1,71 @@
+import requests
+from .api import EndpointBuilder
+
+def all_branches(**kwargs):
+    if "endpoint_builder" not in kwargs:
+        endpoint_builder = EndpointBuilder()
+    else:
+        endpoint_builder = kwargs["endpoint_builder"]
+    
+    response = requests.get(
+        endpoint_builder.base_branch_url,
+        headers=endpoint_builder.headers
+    )
+
+    if response.status_code == 200:
+        return {"success": True, "content": response.json()}
+    else:
+        return {"success": False, "content": response.content}
+
+
+def single_branch(branch: str, includeInheritedMetadata: bool =False, **kwargs):
+    if "endpoint_builder" not in kwargs:
+        endpoint_builder = EndpointBuilder()
+    else:
+        endpoint_builder = kwargs["endpoint_builder"]
+    endpoint_builder.set_branch(branch)
+
+    url = endpoint_builder.with_parameters(
+        endpoint_builder.branch_url,
+        parameters={"includeInheritedMetadata": includeInheritedMetadata}
+    )
+
+    response = requests.get(
+        url,
+        headers=endpoint_builder.headers
+    )
+
+    if response.status_code == 200:
+        return {"success": True, "content": response.json()}
+    else:
+        return {"success": False, "content": response.content}
+
+
+def branch_children(branch: str,  immediateChildren: bool = True, page:int = 0, size: int = 100, **kwargs):
+    if "endpoint_builder" not in kwargs:
+        endpoint_builder = EndpointBuilder()
+    else:
+        endpoint_builder = kwargs["endpoint_builder"]
+    
+    endpoint_builder.set_branch(branch)
+
+    url = endpoint_builder.with_parameters(
+        endpoint_builder.branch_url + "/children",
+        parameters={
+            "immediateChildren": immediateChildren,
+            "page": page,
+            "size": size
+        }
+    )
+
+    print(url)
+
+    response = requests.get(
+        url,
+        headers=endpoint_builder.headers
+    )
+
+    if response.status_code == 200:
+        return {"success": True, "content": response.json()}
+    else:
+        return {"success": False, "content": response.content}
